5b231b721e580b59751388cd5eff5295ca950551
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2023-08-24 19:28:37 -0300

    first commit

--- POO/src/poo/q1Nataaal.java
+++ POO/src/poo/q1Nataaal.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package poo170823;
+package poo;
 
 import java.util.Scanner;
 
